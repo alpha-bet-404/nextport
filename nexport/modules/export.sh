653353a6fd840d389adeadeb714a6aec1cac4f51
alpha-bet-404 <308525416+alpha-bet-404@users.noreply.github.com>
2026-09-25 04:15:13 -0700

    Remove copyright comments from export.sh
    Removed copyright information from export.sh

--- nexport/modules/export.sh
+++ nexport/modules/export.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2026 Steven Osama (zeroman). All rights reserved.
-# Lead Developer: Steven Osama | GitHub: @zeroman-root
+
 
 NEXPORT_EXPORT_DIR="${HOME}/.nexport/exports"
 
